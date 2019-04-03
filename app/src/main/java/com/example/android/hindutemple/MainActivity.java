13119c09313fe82d16e648825e493d38ca6238ed
Asha.Ramanjanaiah <asha.ramanjanaiah@gmail.com>
2019-04-03 06:34:43 +0530

    Creating MainActivity and adding a Menu option. Adding colors for the app.

---
+++ app/src/main/java/com/example/android/hindutemple/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.android.hindutemple;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.admin_login){
+            Intent intent = new Intent(this, AdminLoginActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
